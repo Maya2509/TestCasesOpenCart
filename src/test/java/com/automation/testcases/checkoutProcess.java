e044858ce8911f43e227e53ddc18200ff1bcddae
Maya2509 <mchaves.lo@outlook.com>
2022-08-17 23:14:36 -0600

    Final Test

--- src/test/java/com/automation/testcases/checkoutProcess.java
+++ src/test/java/com/automation/testcases/checkoutProcess.java
@@ -6,10 +6,11 @@ import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import java.io.File;
 import java.time.Duration;
-
+import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 
 
@@ -36,92 +37,71 @@ public class checkoutProcess {
 
  @Test
   public void testCheckout() throws Exception {
-	 driver.get("http://localhost/opencartsite/index.php");
-    driver.findElement(By.linkText("Desktops")).click();
-    driver.findElement(By.linkText("Mac (1)")).click();
-    driver.get("http://localhost/opencartsite/index.php?route=product/category&language=en-gb&path=20_27");
-    driver.findElement(By.xpath("//img[@alt='iMac']")).click();
-    driver.get("http://localhost/opencartsite/index.php?route=product/product&language=en-gb&path=20_27&product_id=41");
-    driver.findElement(By.id("button-cart")).click();
-    driver.findElement(By.xpath("//div[@id='header-cart']/div/button")).click();
-    driver.findElement(By.xpath("//div[@id='header-cart']/div/ul/li/div/p/a[2]/strong")).click();
-    driver.get("http://localhost/opencartsite/index.php?route=checkout/checkout&language=en-gb");
-    driver.findElement(By.id("input-shipping-firstname")).click();
-    driver.findElement(By.id("input-shipping-firstname")).clear();
-    driver.findElement(By.id("input-shipping-firstname")).sendKeys("jef");
-    driver.findElement(By.id("input-shipping-lastname")).click();
-    driver.findElement(By.id("input-shipping-lastname")).clear();
-    driver.findElement(By.id("input-shipping-lastname")).sendKeys("loa");
-    driver.findElement(By.id("input-shipping-address-1")).click();
-    driver.findElement(By.id("input-shipping-address-1")).clear();
-    driver.findElement(By.id("input-shipping-address-1")).sendKeys("ala");
-    driver.findElement(By.id("input-shipping-city")).click();
-    driver.findElement(By.id("input-shipping-city")).clear();
-    driver.findElement(By.id("input-shipping-city")).sendKeys("san@#");
-    driver.findElement(By.id("input-shipping-postcode")).click();
-    driver.findElement(By.id("input-shipping-postcode")).clear();
-    driver.findElement(By.id("input-shipping-postcode")).sendKeys("548769H@");
-    driver.findElement(By.id("input-shipping-country")).click();
-    new Select(driver.findElement(By.id("input-shipping-country"))).selectByVisibleText("Andorra");
-    driver.findElement(By.id("input-shipping-zone")).click();
-    new Select(driver.findElement(By.id("input-shipping-zone"))).selectByVisibleText("Canillo");
-    driver.findElement(By.id("input-comment")).click();
-    driver.findElement(By.id("input-comment")).clear();
-    driver.findElement(By.id("input-comment")).sendKeys("gh,m");
-    driver.findElement(By.id("button-shipping-address")).click();
-    driver.findElement(By.id("input-shipping-method")).click();
-    new Select(driver.findElement(By.id("input-shipping-method"))).selectByVisibleText("Flat Shipping Rate - $5.00");
-    driver.findElement(By.id("input-payment-method")).click();
-    new Select(driver.findElement(By.id("input-payment-method"))).selectByVisibleText("Cash On Delivery");
-  }
+	 driver.get("http://localhost/opencartsite/index.php?route=common/home&language=en-gb");
+	 driver.manage().window().setSize(new Dimension(954, 1040));
+	    driver.findElement(By.cssSelector(".fa-bars")).click();
+	    driver.findElement(By.linkText("Desktops")).click();
+	    driver.findElement(By.linkText("Mac (1)")).click();
+	    driver.findElement(By.cssSelector(".image .img-fluid")).click();
+	    driver.findElement(By.id("button-cart")).click();
+	    
+	    driver.findElement(By.cssSelector("#top > div.container > div.nav.float-end > ul > li:nth-child(5) > a")).click();
+	    
+
+	    {
+	      WebElement element = driver.findElement(By.cssSelector("#top > div.container > div.nav.float-end > ul > li:nth-child(5) > a"));
+	      Actions builder = new Actions(driver);
+	      builder.moveToElement(element).perform();
+	    }
+	    {
+	      WebElement element = driver.findElement(By.tagName("body"));
+	      Actions builder = new Actions(driver);
+	      builder.moveToElement(element, 0, 0).perform();
+	    }
+	    driver.findElement(By.id("input-guest")).click();
+	    driver.findElement(By.id("input-firstname")).click();
+	    driver.findElement(By.id("input-firstname")).sendKeys("qagroup");
+	    driver.findElement(By.id("input-lastname")).click();
+	    driver.findElement(By.id("input-lastname")).sendKeys("qa");
+	    driver.findElement(By.id("input-email")).click();
+	    driver.findElement(By.id("input-email")).sendKeys("qa@gmail.com");
+	    driver.findElement(By.id("input-shipping-company")).click();
+	    driver.findElement(By.id("input-shipping-company")).sendKeys("GROUP");
+	    driver.findElement(By.id("input-shipping-address-1")).click();
+	    driver.findElement(By.id("input-shipping-address-1")).sendKeys("ala");
+	    driver.findElement(By.id("input-shipping-address-2")).click();
+	    driver.findElement(By.id("input-shipping-address-2")).sendKeys("san@@@");
+	    driver.findElement(By.id("input-shipping-city")).click();
+	    driver.findElement(By.id("input-shipping-city")).sendKeys("Car***64");
+	    driver.findElement(By.id("input-shipping-postcode")).click();
+	    driver.findElement(By.id("input-shipping-postcode")).sendKeys("56479");
+	    
+	    /* driver.findElement(By.id("input-shipping-country")).click();
+	    {
+		      WebElement dropdown = driver.findElement(By.id("input-shipping-country"));
+		      dropdown.findElement(By.xpath("//option[. = 'United Kingdom']")).click();
+		    } */
+	    
+	    
+	    /*driver.findElement(By.cssSelector("#input-shipping-zone")).click();
+	    {
+	      WebElement dropdown = driver.findElement(By.cssSelector("#input-shipping-zone"));
+	      dropdown.findElement(By.xpath("//option[. = 'Anglesey']")).click();
+	    }*/
+	    
+	    
+	    driver.findElement(By.id("input-comment")).click();
+	    driver.findElement(By.id("input-comment")).sendKeys("TESTING");
+	    driver.findElement(By.xpath("//div[@id='content']/div")).click();
+	    driver.findElement(By.xpath("//div[@id='content']/div")).click();
+	    driver.findElement(By.xpath("//div[@id='checkout-payment']/div")).click();
+	      
+  } 
 
 
 
  @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
     driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
-
-
-
- private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-
-
- private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-
-
- private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
   }
 }
--- src/test/java/com/automation/testcases/massiveEmails.java
+++ src/test/java/com/automation/testcases/massiveEmails.java
@@ -7,9 +7,12 @@ import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import java.io.File;
 import java.time.Duration;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import org.apache.commons.io.FileUtils;
 
@@ -25,76 +28,52 @@ public class massiveEmails {
     System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
     driver = new ChromeDriver();
     baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30000));
+    driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(2));
     js = (JavascriptExecutor) driver;
   }
 
   @Test
   public void testMassiveEmails() throws Exception {
-    driver.get("http://localhost/opencartsite/admin/");
-    driver.findElement(By.id("input-username")).click();
-    driver.findElement(By.id("input-username")).clear();
-    driver.findElement(By.id("input-username")).sendKeys("admin");
-    driver.findElement(By.id("input-password")).click();
-    driver.findElement(By.id("input-password")).clear();
-    driver.findElement(By.id("input-password")).sendKeys("admin");
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.xpath("//div[@id='modal-security']/div/div/div/button")).click();
-    driver.findElement(By.xpath("//*[@id=\"button-menu\"]")).click();
-    driver.findElement(By.xpath("//*[@id=\"menu-marketing\"]/a")).click();      
-    driver.findElement(By.xpath("//*[@id=\"collapse-6\"]/li[2]/a")).click();  
-    driver.findElement(By.xpath("/html/body/div[1]/nav/ul/li[7]/ul/li[4]/a"));
-    driver.findElement(By.xpath("/html/body/div[1]/nav/ul/li[7]/ul/li[4]/a")).click();
-    driver.findElement(By.id("input-store")).click();
-    driver.findElement(By.id("input-to")).click();
-    new Select(driver.findElement(By.id("input-to"))).selectByVisibleText("All Customers");
-    driver.findElement(By.id("input-subject")).click();
-    driver.findElement(By.id("input-subject")).clear();
-    driver.findElement(By.id("input-subject")).sendKeys("QA Test");
-    driver.findElement(By.xpath("/html/body/p")).click();
-    driver.findElement(By.xpath("/html/body/p")).sendKeys("QA Test");
-    driver.findElement(By.id("button-send")).click();
+	  driver.get("http://localhost/opencartsite/admin/");
+	    driver.manage().window().setSize(new Dimension(954, 1040));
+	    driver.findElement(By.cssSelector(".row:nth-child(1)")).click();
+	    driver.findElement(By.id("input-username")).click();
+	    driver.findElement(By.id("input-username")).sendKeys("admin");
+	    driver.findElement(By.id("input-password")).click();
+	    driver.findElement(By.id("input-password")).sendKeys("admin");
+	    driver.findElement(By.cssSelector(".btn")).click();
+	    driver.findElement(By.cssSelector(".btn-close")).click();
+	    driver.findElement(By.cssSelector("#button-menu > .fas")).click();
+	    js.executeScript("var evt = document.createEvent('MouseEvents');" + 
+	    "evt.initMouseEvent('click',true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0,null);" 
+	    		+ "arguments[0].dispatchEvent(evt);", driver.findElement(By.cssSelector("#menu-marketing > a")));
+	    driver.findElement(By.cssSelector("#collapse-6 > li:nth-child(4) > a")).click();
+	    driver.findElement(By.id("input-store")).click();
+	    driver.findElement(By.id("input-to")).click();
+	    {
+	      WebElement dropdown = driver.findElement(By.id("input-to"));
+	      dropdown.findElement(By.xpath("//option[. = 'All Customers']")).click();
+	    }
+	    driver.findElement(By.id("input-subject")).click();
+	    driver.findElement(By.id("input-subject")).sendKeys("QA Test");
+	    {
+	      WebElement element = driver.findElement(By.id("cke_39"));
+	      Actions builder = new Actions(driver);
+	      builder.moveToElement(element).perform();
+	    }
+	    driver.switchTo().frame(0);
+	    driver.findElement(By.cssSelector("html")).click();
+	    {
+	      WebElement element = driver.findElement(By.cssSelector(".cke_editable"));
+	      js.executeScript("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<p>Hla Testing</p>'}", element);
+	    }
+	    driver.switchTo().defaultContent();
+	    driver.findElement(By.cssSelector("#button-send > .fas")).click();
+	  
   }
 
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
     driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
     }
   }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
-}
